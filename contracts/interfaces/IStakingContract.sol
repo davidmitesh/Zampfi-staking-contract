a118b508eccadd117915b16646ee0a2ba553ac89
mitesh pandey <excelrock_mitesh@yahoo.com>
2022-04-14 22:08:12 +0545

    boiler code and architecture thought

---
+++ contracts/interfaces/IStakingContract.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+interface IStakingContract {
+ function deposit(uint256 amountToDeposit)
+        external
+        returns (uint256 stakedTokenOut);
+ 
+ function redeem(uint256 stakedTokenAmountToRedeem)
+ external
+        returns (uint256 tokenAmountOut);
+ 
+ function getRate()
+ external
+ view	
+        returns (uint256 rate);
+ 
+ function getTotalDeposit() external view returns (uint256 totalDeposit);
+ 
+ function claim() external returns (uint256 claimedTokenAmount);   
+}
