76d78cd2009eaa9ba27f0a992a6e12c14950ba41
mitesh pandey <excelrock_mitesh@yahoo.com>
2022-04-16 15:40:44 +0545

    documentation added, contract still not made ownable,pausable or nonreentrant

--- contracts/LiquidityStakingContract.sol
+++ contracts/LiquidityStakingContract.sol
@@ -11,15 +11,16 @@ contract LiquidityStakingContract is IStakingContract,ERC20{
     using SafeERC20 for IERC20;
     using SafeERC20 for ERC20;
 
+    //This struct is used to keep the record of the zampAmount that is scheduled to be claimed
     struct ClaimReceipt{
         uint256 zampAmount;
         uint256 coolDownInstant;
     }
 
-    uint256 lastUpdatedBlockNumber;
-    uint256 startBlockNumber;
-    uint256 totalDeposits;
-    IERC20 public zampToken;
+    uint256 lastUpdatedBlockNumber;//stores the last blocknumber upto which the rewards are reflected in the totalDeposits
+    uint256 startBlockNumber;//Initial starting block number from which the first deposit happens in contract
+    uint256 totalDeposits;//Total zamp tokens deposits  in the contract with rewards added upto lastUpdatedBlockNumber
+    IERC20 public zampToken;//The erc20 Zamp token
     mapping(address => ClaimReceipt) Receipts;
 
     constructor(address tokenAddress) ERC20("ZampStakeTokens","stkZamp"){
@@ -31,10 +32,10 @@ contract LiquidityStakingContract is IStakingContract,ERC20{
     override
     view	
     returns (uint256 rate){
-        if (totalSupply() == 0 || lastUpdatedBlockNumber == 0){
+        if (totalSupply() == 0 || lastUpdatedBlockNumber == 0){//initially when no deposit is there or when all the tokens are taken out
             rate = 1;//1 stkZamp Token is equal to 1 Zamp token
         }else{
-            rate = (totalDeposits + (block.number - lastUpdatedBlockNumber))/totalSupply();
+            rate = (totalDeposits + (block.number - lastUpdatedBlockNumber))/totalSupply();// (Total Zamp Tokens / Total stkZamp tokens) -> gives value of how many zamp tokens are equal to 1 stkZamp token
         }
     }
 
@@ -49,12 +50,16 @@ contract LiquidityStakingContract is IStakingContract,ERC20{
                 startBlockNumber = block.number;
             }
 
+            stakedTokenOut = amountToDeposit/getRate();//getting the amount of stakeZamp tokens to be minted to depositer
+
+            //Adding the deposited zamp amount to reflect in totalDeposits and also rewards to that point is reflected
             totalDeposits += (block.number - lastUpdatedBlockNumber) + amountToDeposit;
-            lastUpdatedBlockNumber = block.number;
-            stakedTokenOut = amountToDeposit/getRate();
+            lastUpdatedBlockNumber = block.number;//lastUpdatedBlockNumber updated here
+
+            
             _mint(msg.sender,stakedTokenOut);
 
-            //execute an event
+            //emit an event
         }
 
     
@@ -65,14 +70,21 @@ contract LiquidityStakingContract is IStakingContract,ERC20{
         require(stakedTokenAmountToRedeem>0,"Invalid Amount");
         require(balanceOf(msg.sender) >= stakedTokenAmountToRedeem,"not enough stkTokens to redeem");
         require(Receipts[msg.sender].zampAmount == 0,"First claim the already redeemed amount to redeem further");
-        tokenAmountOut = stakedTokenAmountToRedeem * getRate();
+
+        tokenAmountOut = stakedTokenAmountToRedeem * getRate();//getting the equivalent amount of zamp tokens that are equivalent to the stkZamp tokens submitted for burning
+
+         //Subtracting the equivalent zamp amount to reflect in totalDeposits and also rewards to that point is reflected
         totalDeposits = totalDeposits - tokenAmountOut + (block.number - lastUpdatedBlockNumber);
         lastUpdatedBlockNumber = block.number;
+
+        //buring the stkZamp tokens
         _burn(msg.sender,stakedTokenAmountToRedeem);
+
+        //instead of transferring the zampTokens immediately,setting up a payment receipt and setting cooldown period
         Receipts[msg.sender].zampAmount = tokenAmountOut;
         Receipts[msg.sender].coolDownInstant = block.timestamp + 3600;
 
-        //emit and event
+        //emit an event
     }
 
 
@@ -83,7 +95,7 @@ contract LiquidityStakingContract is IStakingContract,ERC20{
     override
     view 
     returns (uint256 totalDeposit){
-        totalDeposit = totalDeposits - (lastUpdatedBlockNumber - startBlockNumber);
+        totalDeposit = totalDeposits - (lastUpdatedBlockNumber - startBlockNumber);//this returns only the deposits in the totalDeposits which does not include the reward
     }
 
     function claim() 
@@ -93,7 +105,7 @@ contract LiquidityStakingContract is IStakingContract,ERC20{
         claimedTokenAmount = Receipts[msg.sender].zampAmount;
         require(claimedTokenAmount > 0,"No tokens redeemed for claiming");
         require(Receipts[msg.sender].coolDownInstant < block.timestamp,"An hour cooldown period not over");
-        zampToken.safeTransfer(msg.sender,claimedTokenAmount);
+        zampToken.safeTransfer(msg.sender,claimedTokenAmount);//executing the receipt details
         Receipts[msg.sender].zampAmount = 0;
         Receipts[msg.sender].coolDownInstant = 0;
     }
